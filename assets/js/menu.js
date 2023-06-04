16b5edb3a38b175ba7e766059d7cf0b87f33a231
Laura G <62339671+lpgiangrande@users.noreply.github.com>
2023-06-04 19:59:28 +0200

    reorganise project structure

---
+++ assets/js/menu.js
@@ -0,0 +1,12 @@
+let closeIcon = "xmark-solid.svg";
+
+function changeIcon () {  
+    const burger = document.querySelector('.navbar-toggler');
+    let iconContent = document.querySelector('.navbar-toggler-icon');
+    let icon = document.querySelector('.open');
+
+    burger.addEventListener('click', (e) => {    
+            iconContent.src
+        });    
+    }
+changeIcon();
