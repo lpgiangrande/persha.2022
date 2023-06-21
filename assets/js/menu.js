9d40ae88ed3d0ea981c6ce23b15c5fd965ce9655
Laura G <62339671+lpgiangrande@users.noreply.github.com>
2023-06-21 07:08:10 +0200

    effet footer

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,12 +1,28 @@
-let closeIcon = "xmark-solid.svg";
-
-function changeIcon () {  
-    const burger = document.querySelector('.navbar-toggler');
-    let iconContent = document.querySelector('.navbar-toggler-icon');
-    let icon = document.querySelector('.open');
-
-    burger.addEventListener('click', (e) => {    
-            iconContent.src
-        });    
+window.addEventListener('DOMContentLoaded', function() {
+    var footer = document.querySelector('footer');
+    var wrapper = document.querySelector('.wrapper-music');
+    
+    function adjustFooterPosition() {
+      var wrapperHeight = wrapper.offsetHeight;
+      var windowHeight = window.innerHeight;
+      var scrollPosition = window.scrollY || window.pageYOffset;
+  
+      if (wrapperHeight < windowHeight) {
+        footer.style.position = 'fixed';
+        footer.style.bottom = '0';
+        footer.style.left = '0';
+      } else {
+        if (scrollPosition + windowHeight >= wrapperHeight) {
+          footer.style.position = 'static';
+        } else {
+          footer.style.position = 'sticky';
+        }
+      }
     }
-changeIcon();
+  
+    adjustFooterPosition();
+  
+    window.addEventListener('scroll', adjustFooterPosition);
+    window.addEventListener('resize', adjustFooterPosition);
+  });
+  
